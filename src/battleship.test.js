2b06fe7da4a96892ef0feb6966c67afdfbf7c93c
Velthas <damianobattaglia97@gmail.com>
2022-09-30 13:36:24 +0200

    added tests for isSunk methods

--- src/battleship.test.js
+++ src/battleship.test.js
@@ -5,3 +5,20 @@ test('Hit method correctly marks position as hit', () => {
   const hitOffset = myCarrier.hit(2);
   expect(hitOffset).toEqual([0, 0]);
 });
+
+test('A carrier is considered sunk when all 5 positions are hit', () => {
+  const myCarrier = Ship(5);
+  myCarrier.hit(0);
+  myCarrier.hit(1);
+  myCarrier.hit(2);
+  myCarrier.hit(3);
+  myCarrier.hit(4);
+  expect(myCarrier.isSunk()).toBe(true);
+});
+
+test('A carrier is not sunk when only 2 positions are hit', () => {
+  const myCarrier = Ship(5);
+  myCarrier.hit(2);
+  myCarrier.hit(4);
+  expect(myCarrier.isSunk()).toBe(false);
+});
