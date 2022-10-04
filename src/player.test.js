97438566292811264aafd6eff2d10da4e20228ec
Velthas <damianobattaglia97@gmail.com>
2022-10-04 14:59:39 +0200

    added tests for player and CPU

---
+++ src/player.test.js
@@ -0,0 +1,34 @@
+import { Board } from './board';
+import { Player } from './player';
+
+test('Turn is played and correctly registered on board', () => {
+  const enemyBoard = Board();
+  const playerOne = Player(1, enemyBoard);
+  playerOne.playTurn([4, 4]);
+  expect(enemyBoard.getBoard()[4][4].isHit()).toBe(true);
+});
+
+test('Turn is not played because tile is already hit', () => {
+  const enemyBoard = Board();
+  const playerOne = Player(1, enemyBoard);
+  enemyBoard.receiveAttack([4, 4]);
+  playerOne.playTurn([4, 4]);
+  expect(playerOne.turnState()).toBe(true); // If turn was not played it should still be P1 turn
+});
+
+test('Turn is not played because it is not players turn', () => {
+  const enemyBoard = Board();
+  const playerOne = Player(1, enemyBoard);
+  playerOne.turnOver();
+  playerOne.playTurn([4, 4]);
+  expect(enemyBoard.getBoard()[4][4].isHit()).toBe(false);
+});
+
+test('Turn is played randomly for CPU', () => {
+  const playerOneBoard = Board();
+  const cpuPlayer = Player(1, playerOneBoard);
+  cpuPlayer.playRandomMove();
+  // If unplayed tile array length is not 100
+  // it means one move was played
+  expect(playerOneBoard.getUnplayedTiles().length).toBe(99);
+});
