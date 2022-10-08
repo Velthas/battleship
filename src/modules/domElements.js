36cfe02d1aa343e3fd320a951c4a23ede270c9cf
Velthas <damianobattaglia97@gmail.com>
2022-10-08 11:03:25 +0200

    added method to display ships on board & added comments

--- src/modules/domElements.js
+++ src/modules/domElements.js
@@ -90,11 +90,10 @@ const domElements = (function () {
 
   const setFriendlyBoard = function (gameBoard, arrayOfShips) {
     let placedShips = 0; // Serves as a counter
-    let orientation = 'horizontal';
+    let orientation = 'horizontal'; // By default, orientation of ships is horizontal
 
     const verticalShips = arrayOfShips[1]; // This is where we draw the ships from
-    const horizontalShips = arrayOfShips[0];
-    const shipNames = ['Carrier', 'Battleship', 'Destroyer', 'Submarine', 'Patrol'];
+    const horizontalShips = arrayOfShips[0]; // Two arrays give us the complete pool
 
     const placeShipsContainer = document.createElement('div'); // Container to add low opacity backdrop
     placeShipsContainer.classList.add('place-container');
@@ -107,12 +106,14 @@ const domElements = (function () {
     divTitle.textContent = 'PLACE YOUR SHIPS';
     placeShipsDiv.appendChild(divTitle);
 
-    const paraInfo = document.createElement('p'); // This paragraph will be used to name ships
-    paraInfo.textContent = 'Please place your Carrier.';
+    const paraInfo = document.createElement('p'); // This paragraph to provide commentary
+    paraInfo.textContent = 'Please place your Carrier'; // on what ship must be placed
     placeShipsDiv.appendChild(paraInfo);
 
     const sandboxGrid = document.createElement('div'); // This is the board where we are planting ships
-    sandboxGrid.classList.add('mock-grid');
+    sandboxGrid.classList.add('mock-grid'); // it's just a prop to do the placing
+
+    // This loop creates all the tiles and applies event listeners.
     for (let i = 0; i < 100; i++) { // Create all the tiles
       const tile = document.createElement('div');
       tile.classList.add('tile');
@@ -148,21 +149,28 @@ const domElements = (function () {
 
         if (gameBoard.isPositionLegal([y, x], currentShip.getOffsets())) {
           gameBoard.placeShip(currentShip, [y, x]);
+          // Every time a ship is placed, display it on the mock grid
+          displayFleet('.mock-grid', gameBoard);
+          // Increasing this counter allows us to place the subsequent ship
           placedShips += 1;
-          console.log(gameBoard)
-          if (placedShips < 5) {
+
+          if (placedShips < horizontalShips.length) {
             paraInfo.textContent = `Please place your ${horizontalShips[placedShips].getName()}`;
           }
         }
-        if (placedShips === 5) {
-          placeShipsContainer.remove();
+
+        if (placedShips === horizontalShips.length) { // When condition met ships have been placed
+          placeShipsContainer.remove(); // Remove the ship placing form
+          domElements.displayFleet('#player-board', gameBoard); // And display the ships of the friendly board.
+          // If this game is made to be PvP, the query should be made into a parameter and passed.
         }
       });
       sandboxGrid.appendChild(tile);
     }
-    placeShipsDiv.appendChild(sandboxGrid);
 
-    const rotateButton = document.createElement('button');
+    placeShipsDiv.appendChild(sandboxGrid); // When the loop is done, we append the grid
+
+    const rotateButton = document.createElement('button'); // Finally the button to allow ship rotation
     rotateButton.addEventListener('click', () => {
       const previousPreview = document.querySelector('.ship-preview');
       if (previousPreview) previousPreview.remove();
@@ -175,6 +183,22 @@ const domElements = (function () {
     document.querySelector('body').appendChild(placeShipsContainer);
   };
 
+  const displayFleet = function (query, gameBoard) {
+    const chosenBoard = document.querySelector(query);
+    const tiles = Array.from(chosenBoard.querySelectorAll('div.tile'));
+    const board = gameBoard.getBoard();
+
+    for (let i = 0; i < 10; i++) {
+      for (let j = 0; j < 10; j++) {
+        if (board[i][j].hasShip()) {
+          const currentShip = board[i][j].getShip();
+          const shipName = currentShip.reference.getName().toLowerCase();
+          tiles[(i * 10) + j].classList.add(shipName);
+        }
+      }
+    }
+  };
+
   return {
     createGridDivs,
     addEventListenersToEnemyBoard,
@@ -182,6 +206,7 @@ const domElements = (function () {
     deleteExistingBoards,
     createResetDiv,
     setFriendlyBoard,
+    displayFleet,
   };
 }());
 
