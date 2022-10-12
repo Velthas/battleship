7b45e04f97317397e57892bc0701e3027323d702
Velthas <damianobattaglia97@gmail.com>
2022-10-12 13:42:14 +0200

    cleaned up code of AI module

--- src/modules/ai.js
+++ src/modules/ai.js
@@ -1,65 +1,52 @@
 const AI = function (Board) {
-  // A reference to the enemy board to run checks on positions
+  // Reference to the enemy board to run checks
   const enemyBoard = Board;
 
-  // This contains arrays with moves yet to be made in all 4 directions.
+  // Contains moves calculated by AI
   const movesToPlay = [];
 
-  // This will store the index of the set of moves we are currently exploring
-  let currentOffsetIndex; // that way, even when our turn resumes we know where to pick up again
+  // Signals which direction we were trying
+  let currentOffsetIndex;
 
-  // Given that the longest ship is at most 5 in lenght
-  // it theoretically suffices to hit 5 - 1 adjacent positions in any direction
-  // If any of the hits fail, just move on to the next direction.
+  // Moves to try are four tiles in each main direction
   const upwardOffsets = [[-1, 0], [-2, 0], [-3, 0], [-4, 0]];
   const downwardOffsets = [[1, 0], [2, 0], [3, 0], [4, 0]];
   const leftmostOffsets = [[0, -1], [0, -2], [0, -3], [0, -4]];
   const rightmostOffsets = [[0, 1], [0, 2], [0, 3], [0, 4]];
-
   const allOffsets = [upwardOffsets, downwardOffsets, leftmostOffsets, rightmostOffsets];
 
-  // This function populates the moves to play array
-  // Each array it produces has the legal moves for a different direction
-  // that is: up/down/left/right
+  // Creates one moveset array for all main directions
   const calculatePositions = function (position) {
-    const positionY = position[0];
-    const positionX = position[1];
-    // Goes over each set
     allOffsets.forEach((set) => {
       const positionsArray = [];
 
       for (let i = 0; i < set.length; i++) {
-        const offsetY = set[i][0];
-        const offsetX = set[i][1];
-
-        const actualY = positionY + offsetY;
-        const actualX = positionX + offsetX;
+        const actualY = position[0] + set[i][0];
+        const actualX = position[1] + set[i][1];
 
         if (!filterLegal([actualY, actualX])) break;
         positionsArray.push([actualY, actualX]);
       }
-      const legalPositions = positionsArray.filter(filterLegal); // Filter out illegal moves
-      // If the array is not empty, it contains moves that need to be played
-      if (legalPositions.length > 0) movesToPlay.push(legalPositions);
+
+      // Only push if it has at least one move
+      if (positionsArray.length > 0) movesToPlay.push(positionsArray);
     });
   };
 
-  // Helper function for filter method, only returns legal moves
+  // Returns true if a move is legal
   const filterLegal = function (position) {
     if (position[0] >= 0 && position[0] < 10
       && position[1] >= 0 && position[1] < 10) {
-      // If the position is not hit returns true
       return !enemyBoard.getBoard()[position[0]][position[1]].isHit();
     } return false;
   };
 
-  // Signals CPU wether to let AI Handle things or hit random tiles
+  // Informs on state of hasMovesToPlay
   const hasMovesToPlay = function () {
     return movesToPlay.length !== 0;
   };
 
-  // When we miss a hit in a direction or we have gone through
-  // all the moves, we must remove the array with moves from the 'movesToPlay' array.
+  // Removes a moveset from the movesToPlay array
   const removeMovesArray = function (index) {
     movesToPlay.splice(index, 1);
   };
@@ -75,37 +62,32 @@ const AI = function (Board) {
     else currentOffsetIndex = Math.floor(Math.random() * (length - 1));
   };
 
-  // Takes a registered hit as position, and sets up
-  // all we need to attack adjacent tiles.
+  // Populates moves array & select first moveset to try
   const initialize = function (position) {
     calculatePositions(position);
     selectMoveset();
   };
 
-  // It checks when a moveset is empty, and if it is found to be, removes it
-  // and replaces it with the next one
+  // Removes empty movesets
   const checkMovesetLength = function () {
     if (movesToPlay[currentOffsetIndex].length === 0) {
       removeMovesArray(currentOffsetIndex);
-      selectMoveset(); // Selects any of the available movesets
+      selectMoveset();
     }
   };
 
   const getMove = function () {
     const move = movesToPlay[currentOffsetIndex][0];
-    const moveHit = doesMoveHit(move);
 
-    if (moveHit) {
-      movesToPlay[currentOffsetIndex].shift(); // remove the first move so the next can be processed
-      checkMovesetLength(); // If the moveset is empty, just remove it
-      return move; // give it back to the player module so it can be played
+    if (doesMoveHit(move)) {
+      movesToPlay[currentOffsetIndex].shift(); // Remove move we are about to play
+      checkMovesetLength(); // Change moveset if current has no more moves
+      return move;
     }
 
-    removeMovesArray(currentOffsetIndex); // this direction does not work, so scrap it
-    selectMoveset(); // we randomly decide another direction to try
-    // When there will be no more movesets to try, the CPU will just resume to hit randomly
-    // Until a hit is found, and AI initialized again.
-    return move; // we return the move for the player to play
+    removeMovesArray(currentOffsetIndex); // Move did not hit, thus remove moveset
+    selectMoveset(); // Select other direction to try next
+    return move;
   };
 
   return { hasMovesToPlay, initialize, getMove };
