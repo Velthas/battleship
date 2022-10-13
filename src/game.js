622bbae2e619296edf9b66093966b1b32253ff3e
Velthas <damianobattaglia97@gmail.com>
2022-10-13 14:04:52 +0200

    refactored and improved code for game loop

--- src/game.js
+++ src/game.js
@@ -7,67 +7,20 @@ const Game = (function () {
   let playerGameBoard;
   let enemyGameBoard;
 
-  let Protagonist;
+  let User;
   let CPU;
 
   let gameOver = false;
   let winner;
 
-  const playGameTurn = function (coordinate) {
-    if (gameOver) return; // If the game has ended, nothing happens on click
+  const gameIsOver = () => playerGameBoard.allShipsSunk() || enemyGameBoard.allShipsSunk();
 
-    const playerTurnPlayed = Protagonist.playTurn(coordinate); // Will return true if move played
-    if (!playerTurnPlayed) return;
-    domElements.markHit(enemyGameBoard, coordinate, 'enemy-board');
-    Protagonist.turnOver(); // End the P1 turn
-
-    if (gameIsOver()) {
-      declareWinner('Player');
-      endGame();
-      return;
-    }
-
-    CPU.isTurn(); // CPU Turn
-    const playedRandomCoordinate = CPU.playRandomMove(); // Will return true if move played
-    domElements.markHit(playerGameBoard, playedRandomCoordinate, 'player-board');
-    CPU.turnOver();
-
-    if (gameIsOver()) {
-      declareWinner('CPU');
-      endGame();
-      return;
-    }
-
-    Protagonist.isTurn(); // Hand over turn to player
-  };
-
-  // Looks at wether all ships have been sunk or all moves made
-  // If any of the conditions is true, returns true.
-  const gameIsOver = function () {
-    if (
-      playerGameBoard.allShipsSunk()
-      || enemyGameBoard.allShipsSunk()
-      || playerGameBoard.getUnplayedTiles().length === 0
-      || enemyGameBoard.getUnplayedTiles().length === 0
-    ) {
-      return true;
-    }
-
-    return false;
-  };
-  // Creates new players/boards and resets gameOver variable
   const resetGameState = function () {
     playerGameBoard = Board();
     enemyGameBoard = Board();
-
-    Protagonist = Player(1, playerGameBoard, enemyGameBoard);
-    CPU = Player(2, enemyGameBoard, playerGameBoard);
-
+    User = Player(true, playerGameBoard, enemyGameBoard);
+    CPU = Player(false, enemyGameBoard, playerGameBoard);
     gameOver = false;
-  };
-
-  const declareWinner = function (player) {
-    winner = player;
   };
 
   const startGame = function () {
@@ -76,16 +29,32 @@ const Game = (function () {
     domElements.createGridDivs();
     domElements.addEventListenersToEnemyBoard(playGameTurn);
     CPU.placeShipsRandomly();
-    domElements.setFriendlyBoard(playerGameBoard, Protagonist.createUserShips());
+    domElements.setFriendlyBoard(playerGameBoard);
   };
 
   const endGame = function () {
-    gameOver = true; // Officially ends the game
-    const playerWon = winner === 'Player';
-    domElements.createResetDiv(playerWon, startGame);
+    gameOver = true;
+    winner = User.turnState() === true ? 'CPU' : 'Player';
+    domElements.createResetDiv(winner, startGame);
   };
+
+  const playGameTurn = function (coordinate) {
+    if (gameOver) return;
+
+    User.playTurn(coordinate); // User Turn
+    if (User.turnState()) return;
+    domElements.markHit(enemyGameBoard, coordinate, 'enemy-board');
+    if (gameIsOver()) return endGame();
+
+    CPU.isTurn(); // CPU Turn
+    const CPUMove = CPU.playRandomMove();
+    domElements.markHit(playerGameBoard, CPUMove, 'player-board');
+    if (gameIsOver()) return endGame();
+
+    User.isTurn();
+  };
+
+  startGame();
 
   return { startGame };
 }());
-
-Game.startGame();
